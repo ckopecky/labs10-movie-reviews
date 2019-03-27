cd854997c6d5984198715512ac13cc93c3e2aeaa
ckopecky <kopecky12112@gmail.com>
2019-03-27 15:20:17 -0700

    formatting

--- client/src/components/home/Hero/SearchQueryHeader.js
+++ client/src/components/home/Hero/SearchQueryHeader.js
@@ -1,32 +1,29 @@
 import React from 'react';
 import './Hero.css';
 
-const SearchQueryHeader = (props) => {
-    const searchResultsLength = () => {
-        return props.searchResults.length  === 1 ? 
-            <div>
-                {`  (${props.searchResults.length } result)`}
-            </div>    
-        :
-        
-        props.searchResults.length === 0 ?
-            <div></div>
-        :
-            <div> 
-                {`  (${props.searchResults.length } results)`}
-            </div>
+const SearchQueryHeader = props => {
+  const searchResultsLength = () => {
+    if (props.searchResults.length === 1) {
+      return <div>{`  (${props.searchResults.length} result)`}</div>;
+    } else if (props.searchResults.length === 0) {
+      return ' ';
+    } else {
+      return <div>{`  (${props.searchResults.length} results)`}</div>;
     }
-    return (
-        <h1 className="search-h1">
-            <div className="search-results-query">
-                  <h5 className="search-label">{props.searchResults.length > 0 ? "Search Results for:": ""}</h5> 
-                  <div className="search-results-length">
-                    <h5 className="query"> {`${props.searchInput}`}</h5>
-                    <h5 className="result-length">{searchResultsLength()}</h5>
-                  </div>
-            </div>
-        </h1>
-    );
+  };
+  return (
+    <h1 className="search-h1">
+      <div className="search-results-query">
+        <h5 className="search-label">
+          {props.searchResults.length > 0 ? 'Search Results for:' : ''}
+        </h5>
+        <div className="search-results-length">
+          <h5 className="query"> {`${props.searchInput}`}</h5>
+          <h5 className="result-length">{searchResultsLength()}</h5>
+        </div>
+      </div>
+    </h1>
+  );
 };
 
 export default SearchQueryHeader;
