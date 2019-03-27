fa51c3d7ecb90a199836c4e0ecaaceb332026495
ckopecky <kopecky12112@gmail.com>
2019-03-27 14:56:45 -0700

    refactor search query header

--- client/src/components/home/Hero/SearchQueryHeader.js
+++ client/src/components/home/Hero/SearchQueryHeader.js
@@ -2,25 +2,27 @@ import React from 'react';
 import './Hero.css';
 
 const SearchQueryHeader = (props) => {
-
     const searchResultsLength = () => {
-        if(props.resultLength === 0) {
-            return "No movies available with that search term. Check your spelling or try a new search!"
-        }
-        else if(props.resultLength === 1) {
-            return props.resultLength + ' result';
-        }
-        else {
-            return props.resultLength + ' results';
-        }
+        return props.searchResults.length  === 1 ? 
+            <div>
+                {`  (${props.searchResults.length } result)`}
+            </div>    
+        :
+        
+        props.searchResults.length === 0 ?
+            <div></div>
+        :
+            <div> 
+                {`  (${props.searchResults.length } results)`}
+            </div>
     }
     return (
-        <h1 className={props.className}>
+        <h1 className="search-h1">
             <div className="search-results-query">
-                  <h5 className="search-label">{props.headerLabel}</h5> 
+                  <h5 className="search-label">{props.searchResults.length > 0 ? "Search Results for:": ""}</h5> 
                   <div className="search-results-length">
-                    <h5 className="query"> {props.searchCriteria}</h5>
-                    <h5 className="result-length">({searchResultsLength()})</h5>
+                    <h5 className="query"> {`${props.searchInput}`}</h5>
+                    <h5 className="result-length">{searchResultsLength()}</h5>
                   </div>
             </div>
         </h1>
